d4117fce19cfe00b2c37663b99cd713f52d9f335
abdulrahman <abdulrahmanazam58@gmail.com>
2024-06-27 10:05:04 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+// Assalamualaikum @everyone 👋
+// 🚀 Day 40 Challenge: Start Coding! 🚀
+// Question 118: Write a loop that logs numbers from 1 to 10 to the console.
+for (var i = 1; i <= 10; i++) {
+    console.log(i);
+}
+console.log("\n");
+// Question 119: Create a while loop that logs "Hello, World!" 5 times.
+var count = 0;
+while (count < 5) {
+    console.log("Hello, World!");
+    count++;
+}
+console.log("\n");
+// Question 120: Use a for...of loop to iterate through an array of your favorite movies and log each one.
+var favoriteMovies = [
+    "Inception",
+    "The Matrix",
+    "The Prestige"
+];
+for (var _i = 0, favoriteMovies_1 = favoriteMovies; _i < favoriteMovies_1.length; _i++) {
+    var movie = favoriteMovies_1[_i];
+    console.log(movie);
+}
